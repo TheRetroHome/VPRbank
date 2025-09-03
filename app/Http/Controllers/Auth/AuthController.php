ec2eb7e5632a508af5cc9d357267b9ac3db3e5c9
armoniooslo <armoniooslo@gmail.com>
2025-09-03 09:41:46 +0000

    started validation rules for authcontroller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function getAuth(){
+        return view('AuthPages.login');
+    }
+
+    public function postAuth(Request $LoginRequest){
+
+    }
+
+    public function postRegister(Request $RegisterRequest){
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('authorization')->group(function(){
+        Route::get('/', [AuthController::class, 'getAuth'])->name('getAuth');
+ });
+
